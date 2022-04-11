1997933d29052107fa9c8cf48425e3836efd4e31
Arda Satata <ardasatata@gmail.com>
2022-04-12 03:27:18 +0800

    update client

--- client/src/app/pages/RecordVideo/index.tsx
+++ client/src/app/pages/RecordVideo/index.tsx
@@ -14,9 +14,57 @@ import { Masthead } from '../HomePage/Masthead';
 import { Features } from '../HomePage/Features';
 import VideoRecorder from 'react-video-recorder';
 
+import { FileUploader } from "react-drag-drop-files";
+import {useState} from "react";
+import {ArrowUpOutlined, ClearOutlined, CloseOutlined, PlayCircleOutlined} from "@ant-design/icons";
+import axios from "axios";
+
 interface Props {}
 
+const fileTypes = ["MP4","MOV"];
+
 export function RecordVideo(props: Props) {
+
+  const [file, setFile] = useState<any>(null);
+  const handleChange = async (file) => {
+    setFile(file);
+  };
+
+  const clearFile = () => {
+    setFile(null);
+  };
+
+  const uploadFile = async () => {
+
+    let videoFile = file[0];
+
+    var videoUrl = URL.createObjectURL(videoFile);
+
+    console.log(videoUrl)
+
+    let blob = await fetch(videoUrl).then(r => r.blob());
+
+    console.log(blob)
+
+    // var reader = new FileReader();
+
+    // @ts-ignore
+    // console.log(reader.read(blobVideo))
+
+    const headers = {
+      'Content-Type': 'multipart/form-data',
+      "Access-Control-Allow-Origin": "*"
+    }
+
+    const formData = new FormData();
+    // @ts-ignore
+    formData.append('video', blob, file[0].name)
+
+    const result = await axios.post("http://140.115.51.243:5000/send-video", formData, { headers })
+
+    console.log(result)
+  };
+
   return (
     <>
       <Helmet>
@@ -28,37 +76,69 @@ export function RecordVideo(props: Props) {
       </Helmet>
       <NavBar />
       <PageWrapperMain>
-        <div className={'flex h-full w-3/4'}>
-          <VideoRecorder
-            isReplayingVideo={false}
-            showReplayControls={true}
-            isOnInitially={true}
-            // countdownTime={RECORDER_TIME_CONFIG.countdownTime}
-            // timeLimit={RECORDER_TIME_CONFIG.timeLimit}
-            mimeType="video/webm;codecs=vp8,opus"
-            // onRecordingComplete={async (videoBlob) => {
-            //   // Do something with the video...
-            //   // setPrediction(['Uploading your video'])
-            //   await submitVideo(videoBlob)
-            //   sum(5, 6)
-            //   console.log('videoBlob', videoBlob)
-            // }}
-            // onStartRecording={() => {
-            //   setPrediction(['Recording video...'])
-            // }}
-            // onTurnOnCamera={
-            //   () => {
-            //     setPrediction(['Press Record First!'])
-            //   }
-            // }
-          />
+        <div className={'flex h-full w-full'}>
+          {/*<VideoRecorder*/}
+          {/*  isReplayingVideo={false}*/}
+          {/*  showReplayControls={true}*/}
+          {/*  isOnInitially={true}*/}
+          {/*  // countdownTime={RECORDER_TIME_CONFIG.countdownTime}*/}
+          {/*  // timeLimit={RECORDER_TIME_CONFIG.timeLimit}*/}
+          {/*  mimeType="video/webm;codecs=vp8,opus"*/}
+          {/*  // onRecordingComplete={async (videoBlob) => {*/}
+          {/*  //   // Do something with the video...*/}
+          {/*  //   // setPrediction(['Uploading your video'])*/}
+          {/*  //   await submitVideo(videoBlob)*/}
+          {/*  //   sum(5, 6)*/}
+          {/*  //   console.log('videoBlob', videoBlob)*/}
+          {/*  // }}*/}
+          {/*  // onStartRecording={() => {*/}
+          {/*  //   setPrediction(['Recording video...'])*/}
+          {/*  // }}*/}
+          {/*  // onTurnOnCamera={*/}
+          {/*  //   () => {*/}
+          {/*  //     setPrediction(['Press Record First!'])*/}
+          {/*  //   }*/}
+          {/*  // }*/}
+          {/*/>*/}
+          <div className={"flex flex-col items-center justify-center text-white w-full"}>
+            {
+              file !== null ? <h1 className={"text-5xl mb-12"}>Your file is ready to upload!</h1> : <h1 className={"text-5xl mb-12"}>Please Drag a video file below!</h1>
+            }
+            {
+              file !== null ?
+                <div className={"flex flex-row"}>
+                  <div
+                    className={
+                      'px-4 text-white text-4xl py-4 transition ease-in-out bg-teal-500 hover:bg-indigo-700 duration-300 items-center justify-center flex cursor-pointer'
+                    }
+                    onClick={uploadFile}
+                  >
+                    <ArrowUpOutlined className={'mr-2'} />
+                    {'UPLOAD'}
+                  </div>
+                  <div
+                    className={
+                      'ml-4 px-4 text-white text-4xl py-4 transition ease-in-out bg-red-400 hover:bg-indigo-700 duration-300 items-center justify-center flex cursor-pointer'
+                    }
+                    onClick={clearFile}
+                  >
+                    <CloseOutlined className={'mr-2'} />
+                    {'CLEAR'}
+                  </div>
+                </div>
+                :
+                <FileUploader
+                  multiple={true}
+                  handleChange={handleChange}
+                  name="file"
+                  types={fileTypes}
+                />
+            }
+            <p className={"mt-6"}>{file ? `File name: ${file[0].name}` : "no files uploaded yet"}</p>
+          </div>
+
         </div>
-        <div className={'h-full flex bg-white w-1/4'}>cok</div>
-        {/*<PageWrapperMain>*/}
-        {/*  <Masthead />*/}
-        {/*  /!*<div className={'bg-white h-full flex flex-1'}>COK</div>*!/*/}
-        {/*  /!*<Features />*!/*/}
-        {/*</PageWrapperMain>*/}
+        {/*<div className={'h-full flex bg-white w-1/4'}>cok</div>*/}
       </PageWrapperMain>
     </>
   );
