e84c576e90b30b90c2fbf90847cecf42ce1a19d8
minelab-ubuntu <a@a.com>
2022-04-25 21:35:07 +0800

    update server.py changes added util.py

---
+++ server/util.py
@@ -0,0 +1,9 @@
+import base64
+import numpy as np
+import cv2
+
+def readb64(uri):
+   encoded_data = uri.split(',')[1]
+   nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+   img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+   return img
